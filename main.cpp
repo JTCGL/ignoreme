73174d0a42696bd9062d99aafce2dc62897149e9
JTCGL <jesse_thoe@hotmail.com>
2020-03-09 03:41:27 -0500

    Added files

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 //lolwut? Pull request!
 
-#include <iostream>
+//#include <iostream>
 //more!
+#include "added.h"
 
 int main() {
 	std::cout << "Edited from C4droid AND vs! Dude! Now sync!" << "\n";
 	std::cout << "Edited from browser github!" << "\n";
 	std::cout << "Voodoo!";
+
+	added();
+
   return 0;
 }
