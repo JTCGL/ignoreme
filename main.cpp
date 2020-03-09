ea5a995fcc6065e91dd3d1c6cf009a0a44a84afc
Jesse <jesse_thoe@hotmail.com>
2020-03-09 17:56:19 -0500

    Edit from openhub browser

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ int main() {
 	std::cout << "Edited from C4droid AND vs! Dude! Now sync!" << "\n";
 	std::cout << "Edited from browser github!" << "\n";
 	std::cout << "Voodoo!";
+std::cout << "edit from OpenHub browser";
 
 	added();
 
