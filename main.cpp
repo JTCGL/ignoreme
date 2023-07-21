f5c0350c8e8954bfbce0e7c12b9267acb959c95e
jesse_thoe@hotmail.com <jesse_thoe@hotmail.com>
2023-07-21 16:09:03 -0500

    yarr

--- main.cpp
+++ main.cpp
@@ -1,10 +1,11 @@
 //lolwut? Pull request!
+//need to add proper makefile for this to work!
 
 #include <iostream>
 //more!
 
 int main() {
-	std::cout << "Edited from mgit.." << "\n";
+	std::cout << "Edited from cxxdroid"<< "\n";
   return 0;
   //;;&"&_?(*
 }
