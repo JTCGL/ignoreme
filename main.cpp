b362ead0bf4075acb7f031bf1ec10905b3258ee2
Jesse <jesse_thoe@hotmail.com>
2023-07-22 12:30:00 -0500

    nums

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "added.h"
+#include "glm\glm.hpp"
 
 int main() {
+	glm::vec3 myvec(1.2f, 3.4f, 5.6);
 	std::cout << "Edited from cxxdroid, VS studio and VS code"<< std::endl;
-	printf("sdasda\n");
+	printf("sdasda, %f %f %f\n",myvec.x, myvec.y, myvec.z);
 	added();
 	return 0;
 }
