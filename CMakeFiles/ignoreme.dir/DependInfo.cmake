da8cdf921e5e431f2b934756e79ce23fac5f8193
JTCGL <jesse_thoe@hotmail.com>
2025-08-05 14:48:09 -0500

    Last Sync: 2025-08-05 14:48 (Mobile)

---
+++ CMakeFiles/ignoreme.dir/DependInfo.cmake
@@ -0,0 +1,39 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/storage/emulated/0/repos/glad/glad.c" "CMakeFiles/ignoreme.dir/glad/glad.c.o" "gcc" "CMakeFiles/ignoreme.dir/glad/glad.c.o.d"
+  "/storage/emulated/0/repos/imgui/imgui.cpp" "CMakeFiles/ignoreme.dir/imgui/imgui.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/imgui/imgui.cpp.o.d"
+  "/storage/emulated/0/repos/imgui/imgui_demo.cpp" "CMakeFiles/ignoreme.dir/imgui/imgui_demo.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/imgui/imgui_demo.cpp.o.d"
+  "/storage/emulated/0/repos/imgui/imgui_draw.cpp" "CMakeFiles/ignoreme.dir/imgui/imgui_draw.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/imgui/imgui_draw.cpp.o.d"
+  "/storage/emulated/0/repos/imgui/imgui_impl_opengl3.cpp" "CMakeFiles/ignoreme.dir/imgui/imgui_impl_opengl3.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/imgui/imgui_impl_opengl3.cpp.o.d"
+  "/storage/emulated/0/repos/imgui/imgui_impl_sdl.cpp" "CMakeFiles/ignoreme.dir/imgui/imgui_impl_sdl.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/imgui/imgui_impl_sdl.cpp.o.d"
+  "/storage/emulated/0/repos/imgui/imgui_impl_sdlrenderer.cpp" "CMakeFiles/ignoreme.dir/imgui/imgui_impl_sdlrenderer.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/imgui/imgui_impl_sdlrenderer.cpp.o.d"
+  "/storage/emulated/0/repos/imgui/imgui_tables.cpp" "CMakeFiles/ignoreme.dir/imgui/imgui_tables.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/imgui/imgui_tables.cpp.o.d"
+  "/storage/emulated/0/repos/imgui/imgui_widgets.cpp" "CMakeFiles/ignoreme.dir/imgui/imgui_widgets.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/imgui/imgui_widgets.cpp.o.d"
+  "/storage/emulated/0/repos/imgui/misc/cpp/imgui_stdlib.cpp" "CMakeFiles/ignoreme.dir/imgui/misc/cpp/imgui_stdlib.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/imgui/misc/cpp/imgui_stdlib.cpp.o.d"
+  "/storage/emulated/0/repos/include/GUIDirectoryBrowser.cpp" "CMakeFiles/ignoreme.dir/include/GUIDirectoryBrowser.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/include/GUIDirectoryBrowser.cpp.o.d"
+  "/storage/emulated/0/repos/include/GUIFileBrowser.cpp" "CMakeFiles/ignoreme.dir/include/GUIFileBrowser.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/include/GUIFileBrowser.cpp.o.d"
+  "/storage/emulated/0/repos/include/GUILauncher.cpp" "CMakeFiles/ignoreme.dir/include/GUILauncher.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/include/GUILauncher.cpp.o.d"
+  "/storage/emulated/0/repos/include/GUIMessageDlg.cpp" "CMakeFiles/ignoreme.dir/include/GUIMessageDlg.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/include/GUIMessageDlg.cpp.o.d"
+  "/storage/emulated/0/repos/include/GUIShaderEdit.cpp" "CMakeFiles/ignoreme.dir/include/GUIShaderEdit.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/include/GUIShaderEdit.cpp.o.d"
+  "/storage/emulated/0/repos/include/GUITestbed.cpp" "CMakeFiles/ignoreme.dir/include/GUITestbed.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/include/GUITestbed.cpp.o.d"
+  "/storage/emulated/0/repos/include/glres.cpp" "CMakeFiles/ignoreme.dir/include/glres.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/include/glres.cpp.o.d"
+  "/storage/emulated/0/repos/include/glshader.cpp" "CMakeFiles/ignoreme.dir/include/glshader.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/include/glshader.cpp.o.d"
+  "/storage/emulated/0/repos/include/platform.cpp" "CMakeFiles/ignoreme.dir/include/platform.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/include/platform.cpp.o.d"
+  "/storage/emulated/0/repos/main.cpp" "CMakeFiles/ignoreme.dir/main.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/main.cpp.o.d"
+  "/storage/emulated/0/repos/src/triangle_renderer.cpp" "CMakeFiles/ignoreme.dir/src/triangle_renderer.cpp.o" "gcc" "CMakeFiles/ignoreme.dir/src/triangle_renderer.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
